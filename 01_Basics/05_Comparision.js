928de293a4c79653e834417114241942c1f02935
mdaman0109 <aman.md0109@gmail.com>
2025-02-25 20:24:29 +0000

    Comparision- Strict check

--- 01_Basics/05_Comparision.js
+++ 01_Basics/05_Comparision.js
@@ -8,7 +8,7 @@
 // console.log("2" > 1);
 // console.log("02" > 1);
 
-console.log(null > 0);
+console.log(null > 0);  //it will convert null to 0 or NaN unpredictable so it is better to avoid all these type of comparisions
 console.log(null == 0);
 console.log(null >= 0);
 
